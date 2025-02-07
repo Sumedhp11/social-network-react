4cf8c6df4cda131986bf31a7ffe859d03fbe5b95
Sumedhp11 <sumedhpawar8966@gmail.com>
2025-02-07 23:14:04 +0530

    feat: Remove Eruda dependency and clean up App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import { lazy, Suspense, useEffect } from "react";
+import AuthLayout from "@/components/layouts/AuthLayout";
+import { lazy, Suspense } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router";
-import Loader from "./components/ui/Loader";
 import ProtectedRoute from "./components/layouts/ProtectedRoute";
+import Loader from "./components/ui/Loader";
 const Login = lazy(() => import("@/components/pages/auth/Login"));
 const Register = lazy(() => import("@/components/pages/auth/Register"));
-import AuthLayout from "@/components/layouts/AuthLayout";
 const AppLayout = lazy(() => import("@/components/layouts/AppLayout"));
 const Home = lazy(() => import("@/components/pages/main/Home"));
 const UserProfile = lazy(() => import("@/components/pages/main/UserProfile"));
-import eruda from "eruda";
 const App = () => {
-  useEffect(() => {
-    // Initialize Eruda only in development mode
-    if (process.env.NODE_ENV !== "production") {
-      eruda.init();
-    }
-  }, []);
   return (
     <Router>
       <Suspense fallback={<Loader />}>
