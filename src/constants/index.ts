5a6a609b20f534b24f25293da3ef02299195c227
Sumedhp11 <sumedhpawar8966@gmail.com>
2025-03-04 23:38:33 +0530

    fix: Add TURN server configuration with dynamic credentials

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,6 +3,10 @@ import axios from "axios";
 const server_url = import.meta.env.VITE_BACKEND_URL;
 const socket_url = import.meta.env.VITE_SOCKET_URL;
 const stream_server_url = import.meta.env.VITE_STREAM_SERVER_URL;
+const turn_server_url = import.meta.env.VITE_TURN_SERVER_URL;
+const turn_server_username = import.meta.env.VITE_TURN_SERVER_USERNAME;
+const turn_server_password = import.meta.env.VITE_TURN_SERVER_PASSWORD;
+
 const axiosInstance = axios.create({
   baseURL: server_url,
   withCredentials: true,
@@ -28,11 +32,16 @@ const socketEvents = {
 };
 const ICE_SERVERS: RTCConfiguration = {
   iceServers: [
-    { urls: "stun:119.18.55.92:3478" },
     {
-      urls: "turn:119.18.55.92:3478",
-      username: "bn_turn",
-      credential: "bn_2_0",
+      urls: "stun:stun.l.google.com:19302",
+    },
+    {
+      urls: [
+        `turn:${turn_server_url}?transport=udp`,
+        `turn:${turn_server_url}?transport=tcp`,
+      ],
+      username: turn_server_username,
+      credential: turn_server_password,
     },
   ],
 };
