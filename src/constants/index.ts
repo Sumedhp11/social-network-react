0fa9eb75b9b8b941f0d7d74aa3cbaf81c6be958d
Sumedhp11 <sumedhpawar8966@gmail.com>
2025-03-09 14:58:58 +0530

    fix: Update ICE transport policy to use relay for improved security

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -39,7 +39,7 @@ const ICE_SERVERS: RTCConfiguration = {
       credential: turn_server_password,
     },
   ],
-  iceTransportPolicy: "all",
+  iceTransportPolicy: "relay",
   bundlePolicy: "balanced" as RTCBundlePolicy,
   rtcpMuxPolicy: "require",
   iceCandidatePoolSize: 0,
